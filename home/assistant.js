d76e74c819895bf90242bb11282b93d2bfa82370
aebabis <aebabis@gmail.com>
2022-04-14 19:27:39 -0700

    Improved and simplified assistant

--- home/assistant.js
+++ home/assistant.js
@@ -1,4 +1,6 @@
 import { logger } from './lib/logger';
+import { nmap } from './lib/nmap';
+import { by } from './lib/util';
 import { disableService } from './lib/planner-api.js';
 
 const RUN_FILE = '/run/assistant.txt';
@@ -14,148 +16,64 @@ const usage = (ns) => {
 		`  ./${script} help\n`;
 }
 
+const canDirectConnect = (ns, hostname) => {
+	const { isConnectedTo, backdoorInstalled } = ns.getServer(hostname);
+	return hostname === 'home' || isConnectedTo || backdoorInstalled;
+}
+
+const getPathTo = (ns, hostname) => {
+	if (ns.getServer(hostname).isConnectedTo)
+		return [];
+	const next = {};
+	const visited = [hostname];
+	const path = (start) => start === hostname ? [hostname] : [start, ...path(next[start])];
+	for (let i = 0; i < visited.length; i++) {
+		const neighbors = ns.scan(visited[i]).filter(s=>!visited.includes(s));
+		for (const neighbor of neighbors) {
+			visited.push(neighbor);
+			next[neighbor] = visited[i];
+			const { isConnectedTo, backdoorInstalled } = ns.getServer(neighbor);
+			if (isConnectedTo)
+				return path(visited[i]);
+			else if (neighbor === 'home' || backdoorInstalled)
+				return path(neighbor);
+		}
+	}
+}
+
+const getPath = (ns) => {
+	const skill = ns.getHackingLevel();
+	const servers = nmap(ns)
+		.map(ns.getServer)
+		.filter(server => server.hasAdminRights)
+		.filter(server => !server.purchasedByPlayer)
+		.filter(server => !server.backdoorInstalled)
+		.filter(server => server.requiredHackingSkill <= skill);
+
+	const questTarget = servers.find(server => PRIORITIES.includes(server.hostname));
+	
+	if (questTarget != null)
+		return getPathTo(ns, questTarget.hostname);
+	else
+		return servers.map(server => getPathTo(ns, server.hostname)).sort(by('length'))[0];
+}
+
 /** @param {NS} ns **/
 const runDaemon = async (ns) => {
 	const console = logger(ns);
-	const visited = ["home"];
-	const agenda = [];
-	for (let i = 0; i < visited.length; i++) {
-		let entrypoint = visited[i];
-		ns.scan(entrypoint).forEach(target => {
-			if (!visited.includes(target)) {
-				visited.push(target);
-				agenda.push([entrypoint, target])
-			}
-		});
-	}
-	// ns.print(agenda.map(l=>l.join(', ')).join('\n'));
-	// return
 
-	// Get shortest array of manual server hops to end target
-	const backtrack = (end) => {
-		const server = ns.getServer(end);
-		if (server.purchasedByPlayer || server.backdoorInstalled)
-			return [end];
-		const [entrypoint] = agenda.find(([, target]) => target === end);
-		const path = backtrack(entrypoint);
-		path.push(end);
-		return path;
-	}
-
-	const priorities = PRIORITIES.filter(p=>visited.includes(p)).map(target => backtrack(target)).flat();
-	const byPriority = (a,b)=>priorities.includes(b[b.length-1])-priorities.includes(a[a.length-1]);
-
-	const removeCompleted = () => {
-		for (let i = 0; i < agenda.length; i++) {
-			const [, target] = agenda[i];
-			const server = ns.getServer(target);
-			if (server.purchasedByPlayer || server.backdoorInstalled) {
-				agenda.splice(i, 1);
-				i--;
-			}
-		}
-	};
-
-	const getNextTarget = () => {
-		const currentServer = visited.find(hostname => ns.getServer(hostname).isConnectedTo);
-		const skill = ns.getHackingLevel();
-		const available = agenda.filter(([entry, target]) => {
-			const { hasAdminRights, requiredHackingSkill } = ns.getServer(target);
-			// const canApproach = ns.getServer(entry).hasAdminRights;
-			return hasAdminRights && requiredHackingSkill <= skill;
-			// return canApproach && canBackdoor;
-		}).map(([,t])=>backtrack(t))      // Convert each edge to a path
-
-		const immediateTarget = available
-			.find(list=>list[list.length-1] === currentServer);
-
-		if (immediateTarget)
-			return immediateTarget;
-
-	    available.sort((a,b)=>b.length-a.length) // Prefer long paths to save typing
-		  .sort((a,b)=>(b[0]===currentServer)-(a[0]===currentServer))
-		  .sort(byPriority);              // Story servers come first
-		return available[0];
-	}
-
-	let wentOffPath = false;
+	let previousLocation;
 	let previousTarget;
-
-	let cachedCurrent;
-	let cachedTarget;
-	let cachedHostname;
-	let cachedMessage;
-	const getMessage = (server) => {
-		if (server != null) {
-			const [current, ...rest] = server;
-			const target = rest.pop();
-			const hostname = visited.find(hostname => ns.getServer(hostname).isConnectedTo);
-			// ns.print(cachedCurrent + ' ' + current);
-			// ns.print(cachedTarget + ' ' + target);
-			// ns.print(cachedHostname + ' ' + hostname);
-			if (current === cachedCurrent && target === cachedTarget && hostname === cachedHostname) {
-				return cachedMessage;
-			}
-			cachedCurrent = current;
-			cachedTarget = target;
-			cachedHostname = hostname;
-			let step = 0;
-			const steps = [
-				`connect ${current}`,
-				...rest.map(hostname => `connect ${hostname}`),
-				`connect ${target}`,
-				`backdoor`];
-			if (ns.getServer(target).isConnectedTo) {
-				if (!wentOffPath) {
-					steps[0] = '';
-					if (previousTarget == null) {
-						steps[1] = '';
-					}
-				}
-				step = 2;
-				wentOffPath = false;
-				previousTarget = target;
-			} else if (ns.getServer(current).isConnectedTo) {
-				if (!wentOffPath) {
-					steps[0] = '';
-				}
-				step = 1;
-			} else {
-				const index = rest.findIndex(hostname => ns.getServer(hostname).isConnectedTo);
-				if (index !== -1) {
-					step = index + 2;
-				} else {
-					wentOffPath = true;
-					step = 0;
-				}
-			}
-			const lead = i=>i===step?'> ':'  ';
-			return cachedMessage = steps.map((l,i) =>lead(i)+l).join('\n');
-		} else if (agenda.length === 0) {
-			return cachedMessage = 'All servers backdoored';
-		} else {
-			return cachedMessage = 'No backdoor for current level and programs';
-		}
-	}
-
-	while (agenda.length > 0) {
-		try {
-			removeCompleted();
+	while (true) {
+		const path = getPath(ns);
+		if (path != null) {
 			ns.clearLog();
-			const server = getNextTarget();
-			const message = getMessage(server);
-			ns.print(message);
-			await ns.write(RUN_FILE, message, 'w');
-		} catch (error) {
-			console.error(error);
-		} finally {
+			ns.print(path.map(s => s === 'home' ? 'home' : 'connect ' + s).join('\n') + '\nbackdoor');
 			await ns.sleep(50);
+		} else {
+			await ns.sleep(5000);
 		}
 	}
-
-	// Prevent restart of service once all servers
-	// have been backdoored
-	disableService(ns, 'assistant');
 }
 
 /** @param {NS} ns **/
