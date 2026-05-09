385eaa5cc9b1fc91f26bc1a6ffe00aa34a3bd247
Alexandra Babis <aebabis@gmail.com>
2026-05-09 13:01:58 -0700

    Join city factions for possible Neuroflux if in gang

--- home/bin/self/aug/join-factions.js
+++ home/bin/self/aug/join-factions.js
@@ -1,16 +1,18 @@
-import { getStaticData } from "../../../lib/data-store";
+import { getStaticData, getGangData } from "../../../lib/data-store";
 import { CITY_FACTIONS } from "./factions";
 
 /** @param {NS} ns */
 export async function main(ns) {
   ns.disableLog("ALL");
   const { cityFaction, targetFaction } = getStaticData(ns);
+  const gang = getGangData(ns)?.gangInfo?.faction;
   const invites = ns.singularity.checkFactionInvitations();
   for (const faction of invites) {
     if (
       faction === cityFaction ||
       faction === targetFaction ||
-      !CITY_FACTIONS.includes(faction)
+      !CITY_FACTIONS.includes(faction) ||
+      gang != null
     )
       ns.singularity.joinFaction(faction);
   }
