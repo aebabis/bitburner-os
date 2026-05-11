2ee708b77c39baa40eee71f31fd398f5de906da4
Alexandra Babis <aebabis@gmail.com>
2026-05-11 11:11:37 -0700

    New data viewer

--- home/data.js
+++ home/data.js
@@ -1,12 +1,31 @@
-import { getPlayerData, getMoneyData } from './lib/data-store.js';
+import {
+  getHostnames,
+  getStaticData,
+  getGangData,
+  getPlayerData,
+  getMoneyData,
+  getRamData,
+  getContractData,
+  getGoalsData,
+} from './lib/data-store.js';
 /** @param {NS} ns */
 export async function main(ns) {
+  const OPTIONS = {
+    hostnames: getHostnames,
+    static: getStaticData,
+    gang: getGangData,
+    player: getPlayerData,
+    money: getMoneyData,
+    ram: getRamData,
+    contracts: getContractData,
+    goals: getGoalsData,
+  };
   const [type] = ns.args;
-  if (type === 'player') {
-    ns.tprint(JSON.stringify(getPlayerData(ns), null, 2));
-  } else if (type === 'money') {
-    ns.tprint(JSON.stringify(getMoneyData(ns), null, 2));
+  if (typeof type === 'string' && Object.hasOwn(OPTIONS, type)) {
+    ns.disableLog('ALL');
+    ns.ui.openTail();
+    ns.print(JSON.stringify(OPTIONS[type](ns), null, 2));
   } else {
-    ns.tprint('USAGE: data [player|money]');
+    ns.tprint(`USAGE: data [ ${Object.keys(OPTIONS).sort().join(' | ')} ]`);
   }
 }
