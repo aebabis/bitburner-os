0677c7475d135df4730c0280e09a232095fc2f84
Alexandra Babis <aebabis@gmail.com>
2026-05-11 06:14:05 -0700

    Making shares automatically die

--- home/bin/workers/share.js
+++ home/bin/workers/share.js
@@ -1,6 +1,11 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  while (true) {
+  const MIN = 5;
+  const MAX = 10;
+  const count = MIN + ~~(Math.random() * (MAX-MIN));
+  // Have share automatically die in-case
+  // master process is RAM-starved
+  for (let c = 0; c < count; c++) {
     await ns.share();
   }
 }
