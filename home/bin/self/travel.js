6db87bc1d4ab332642b4a5d171aaec8ddaab2d4d
Alexandra Babis <aebabis@gmail.com>
2026-05-15 16:53:10 -0700

    Travel bugfix

--- home/bin/self/travel.js
+++ home/bin/self/travel.js
@@ -1,5 +1,11 @@
 /** @param {NS} ns */
 export async function main(ns) {
   ns.disableLog("ALL");
-  ns.singularity.travelToCity(ns.args[0]);
+  const [location] = ns.args;
+  if (typeof location !== 'string' || !Object.values(ns.enums.CityName).includes(location)) {
+    throw new Error('Expected location parameter. Got ' + location);
+  }
+  // Safety check prevents continuous cash drain
+  if (ns.getPlayer().location !== location)
+    ns.singularity.travelToCity(location);
 }
