0a9c3218d7f6febcd33cb5fd331e650e32772efa
Alexandra Babis <aebabis@gmail.com>
2026-05-17 11:05:47 -0700

    Making improvement.js typesafe

--- home/bin/self/improvement.js
+++ home/bin/self/improvement.js
@@ -1,16 +1,24 @@
 import { getGoals, COMBAT_STATS } from "../../lib/goals";
 import { shouldWorkHaveFocus } from "../../lib/query-service";
 import { putPlayerData } from "../../lib/data-store";
-import { by } from "../../lib/util";
+
+/** @param {NS} ns @param {number} levelReq */
+const getStatToTrain = (ns, levelReq) => {
+  const { skills } = ns.getPlayer();
+  const possibleStats = COMBAT_STATS.filter((stat) => levelReq > skills[stat]);
+  let stat = possibleStats[0];
+  for (const other of possibleStats) {
+    if (skills[other] < skills[stat])
+      stat = other;
+  }
+  return stat;
+};
 
 /** @param {NS} ns */
 export async function main(ns) {
   const combatGoal = getGoals(ns).find(g => g.type === "COMBAT_LEVELS" && !g.isDone());
   const levelReq = combatGoal?.requirement ?? 0;
-  const levelsNeeded = COMBAT_STATS.map((stat) => [stat, levelReq - ns.getPlayer().skills[stat]])
-    .filter(([, needed]) => +needed > 0)
-    .sort(by('1'));
-  const statToTrain = levelsNeeded.at(-1)?.[0];
+  const statToTrain = getStatToTrain(ns, levelReq);
   if (statToTrain) {
     const focus = shouldWorkHaveFocus(ns);
     ns.singularity.gymWorkout("Powerhouse Gym", ns.enums.GymType[statToTrain], focus);
