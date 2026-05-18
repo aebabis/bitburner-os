059f9d835e5d5cb7b4f1d8d0d5dd0e4d0e532ccb
Alexandra Babis <aebabis@gmail.com>
2026-05-18 07:21:09 -0700

    Restoring war script

---
+++ home/bin/gang/decide-war.js
@@ -0,0 +1,30 @@
+import { putGangData } from "../../lib/data-store";
+
+/** @param {NS} ns */
+export async function main(ns) {
+  const [gangName] = ns.args;
+
+  const otherGangInformation = ns.gang.getAllGangInformation();
+  const { power, territory } = otherGangInformation[/** @type {string} */ (gangName)];
+  delete otherGangInformation[/** @type {string} */ (gangName)];
+
+  const enemyInfo = Object.entries(otherGangInformation).map(
+    ([faction, info]) => ({
+      faction,
+      ...info,
+      clashWinChance: power / (power + info.power),
+    }),
+  );
+  const livingEnemies = enemyInfo.filter((e) => e.territory > 0);
+  const averageWinChance =
+    livingEnemies.map((e) => e.clashWinChance).reduce((a, b) => a + b, 0) /
+    livingEnemies.length;
+  putGangData(ns, { power, territory, enemyInfo });
+
+  if (territory < 0.99 && averageWinChance > 0.55) {
+    ns.gang.setTerritoryWarfare(true);
+    await ns.sleep(11000);
+  } else {
+    ns.gang.setTerritoryWarfare(false);
+  }
+}
