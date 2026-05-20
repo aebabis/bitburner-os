4ffd2ebeac2dfd06c9bc19a9430e6d15d599510a
Alexandra Babis <aebabis@gmail.com>
2026-05-19 19:03:20 -0700

    Restructuring augment.js

--- home/bin/self/aug/augment.js
+++ home/bin/self/aug/augment.js
@@ -6,7 +6,6 @@ export async function main(ns) {
   const retry = true;
 
   await rmi(ns, retry)("/bin/self/aug/load-faction-favor.js");
-  await rmi(ns, retry)("/bin/self/aug/load-owned-augs.js");
   await rmi(ns, retry)("/bin/self/aug/load-aug-names.js");
   await rmi(ns, retry)("/bin/self/aug/load-aug-prices.js");
   await rmi(ns, retry)("/bin/self/aug/load-aug-reps.js");
@@ -15,7 +14,8 @@ export async function main(ns) {
   await rmi(ns, retry)("/bin/self/aug/load-faction-reqs.js");
 
   while (true) {
-    await rmi(ns)("/bin/self/aug/load-faction-favor-gain.js");
+    await rmi(ns, retry)("/bin/self/aug/load-owned-augs.js");
+    await rmi(ns, retry)("/bin/self/aug/load-faction-favor-gain.js");
     await rmi(ns)("/bin/self/aug/join-factions.js");
     await rmi(ns)("/bin/self/aug/purchase-augs.js");
     await ns.sleep(100);
--- home/bin/self/aug/load-owned-augs.js
+++ home/bin/self/aug/load-owned-augs.js
@@ -3,8 +3,7 @@ import { putPlayerData } from "../../../lib/data-store";
 /** @param {NS} ns */
 export async function main(ns) {
   ns.disableLog("ALL");
-  ns.tprint("Loading Purchased Augmentations");
-
-  const purchasedAugmentations = ns.singularity.getOwnedAugmentations(true);
-  putPlayerData(ns, { purchasedAugmentations });
+  putPlayerData(ns, {
+    purchasedAugmentations: ns.singularity.getOwnedAugmentations(true),
+  });
 }
