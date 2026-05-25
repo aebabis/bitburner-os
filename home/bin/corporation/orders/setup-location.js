b762f56f21189cfdf1f1714e5edd2fca367b20fc
Alexandra Babis <aebabis@gmail.com>
2026-05-24 20:27:59 -0700

    fixup! Making corp script run less

---
+++ home/bin/corporation/orders/setup-location.js
@@ -0,0 +1,20 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  const [
+    divisionName,
+    cityName,
+    needsWarehouse = false,
+  ] = ns.args;
+
+  const { officeInitialCost, warehouseInitialCost } = ns.corporation.getConstants();
+
+  const setupCost = needsWarehouse ?
+    officeInitialCost + warehouseInitialCost :
+    officeInitialCost;
+
+  if (ns.corporation.getCorporation().funds < setupCost) {
+    ns.corporation.expandCity(divisionName, cityName);
+    if (needsWarehouse)
+      ns.corporation.purchaseWarehouse(divisionName, cityName);
+  }
+}
