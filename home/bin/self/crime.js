b0a1e732c08d359472275e282eac0f42fd607121
Alexandra Babis <aebabis@gmail.com>
2026-05-25 19:42:32 -0700

    Giving crime.js a duration limit parameter

--- home/bin/self/crime.js
+++ home/bin/self/crime.js
@@ -2,7 +2,8 @@ import { getPlayerData, putPlayerData } from "../../lib/data-store";
 
 /** @typedef {{name: string, chance: number, time: number, expectedValue: number}} CrimeStat */
 
-const selectCrime = (/** @type {NS} */ ns) => {
+/** @param {NS} ns @param {number|null} maxDuration */
+const selectCrime = (ns, maxDuration) => {
   const { player, crimeStats } = getPlayerData(ns);
   if (crimeStats == null) return "Shoplift";
 
@@ -11,8 +12,9 @@ const selectCrime = (/** @type {NS} */ ns) => {
     return "Homicide";
 
   const PATIENCE = 90 * 1000;
-  const allowedCrimes = crimeStats.filter(
-    (/** @type {CrimeStat} */ c) => c.chance === 1 || c.chance >= c.time / PATIENCE,
+  const allowedCrimes = crimeStats
+    .filter((/** @type {CrimeStat} */ c) => maxDuration == null || c.time * 1000 <= maxDuration)
+    .filter((/** @type {CrimeStat} */ c) => c.chance === 1 || c.chance >= c.time / PATIENCE,
   );
 
   const bestCrime = allowedCrimes.reduce((/** @type {CrimeStat} */ a, /** @type {CrimeStat} */ b) =>
@@ -26,7 +28,9 @@ const selectCrime = (/** @type {NS} */ ns) => {
 export async function main(ns) {
   ns.disableLog("ALL");
 
-  const crime = selectCrime(ns);
+  const maxDuration = +ns.args[0] || null;
+
+  const crime = selectCrime(ns, maxDuration);
   const duration = ns.singularity.commitCrime(crime);
   putPlayerData(ns, { currentWork: ns.singularity.getCurrentWork() });
   await ns.sleep(duration);
