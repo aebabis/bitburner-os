6e130ee93a5c3a93633e8efb32d878e4e285a501
Alexandra Babis <aebabis@gmail.com>
2026-05-25 18:02:48 -0700

    Adding Tobacco company name

--- home/bin/corporation/constants.js
+++ home/bin/corporation/constants.js
@@ -5,6 +5,7 @@ export const BOOST_MATERIALS = ['AI Cores', 'Hardware', 'Real Estate', 'Robots']
 export const DivisionNames = {
   Agriculture: 'Rhizome Foods',
   Chemical: 'Rhizome Derivatives',
+  Tobacco: 'Rhizome Reeds',
   'Water Utilities': 'Taproot Infrastructure',
 };
 
