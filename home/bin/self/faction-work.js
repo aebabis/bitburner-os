e922053a8b216fd1c5177b26cf68cd938182b5b0
aebabis <acbabis@gmail.com>
2026-05-27 16:27:29 -0700

    Preventing flicker

--- home/bin/self/faction-work.js
+++ home/bin/self/faction-work.js
@@ -6,6 +6,9 @@ export async function main(ns) {
   ns.disableLog('ALL');
   const faction = /** @type {string} */ (ns.args[0]);
   const focus = shouldWorkHaveFocus(ns);
+  if (ns.singularity.getCurrentWork()?.factionName === faction) {
+    return;
+  }
   if (
     ns.singularity.workForFaction(faction, 'hacking', focus) ||
     ns.singularity.workForFaction(faction, 'field', focus) ||
