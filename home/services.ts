ab548312be046642404e8770b71ec39b0e130462
aebabis <acbabis@gmail.com>
2026-05-31 17:06:33 -0700

    Updating services.ts

--- home/services.ts
+++ home/services.ts
@@ -1,26 +1,36 @@
+import { KEYWORD } from './lib/colors.ts';
 import {
   getServices,
   enableService,
   disableService,
-  // restartService,
   getTableString,
 } from './lib/service-api.ts';
 
-/** @typedef {{id: number | string, name: string, status: string, pid: number, desc: string}} ServiceEntry */
+const HELP = [
+  ['services enable <name|id> [--force]', 'enables a service'],
+  ['services disable <name|id>', 'disables a service'],
+  ['services tail <name|id>', 'opens tail associated with process'],
+];
+const LEFT_PAD = Math.max(...HELP.map((row) => row[0].length));
 
 export async function main(ns: NS) {
   const flags = ns.flags([['force', false]]);
-  const [command, target] = /** @type {string[]} */ flags._;
-  const force = /** @type {boolean} */ flags.force;
-  if (command == null) ns.tprint('\n' + getTableString(ns, getServices(ns)));
-  else if (command === 'enable') enableService(ns, target, force);
+  const [command, target] = flags._ as string[];
+  const force = flags.force as boolean;
+  if (command == null) {
+    ns.tprint(
+      '\n' +
+        getTableString(ns, getServices(ns)) +
+        '\n' +
+        HELP.map(
+          (row) => KEYWORD(row[0].padEnd(LEFT_PAD)) + '\n  ' + row[1],
+        ).join('\n'),
+    );
+    ('services enable <name|pid> [--force]: enables a service\n');
+  } else if (command === 'enable') enableService(ns, target, force);
   else if (command === 'disable') disableService(ns, target);
-  // else if (command === 'restart')
-  //   restartService(ns, target);
-  else if (command === 'start') enableService(ns, target, force);
-  else if (command === 'stop') disableService(ns, target);
   else if (command === 'tail') {
-    const services = /** @type {ServiceEntry[]} */ getServices(ns);
+    const services = getServices(ns);
     const service = services.find(
       (service) => service.id === target || service.name === target,
     );
@@ -31,5 +41,7 @@ export async function main(ns: NS) {
         `Available services: ${services.map((s) => s.name).join(', ')}`,
       );
     }
+  } else {
+    ns.tprint('Unrecognized command: ' + command);
   }
 }
