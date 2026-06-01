f45c3b28b99d48fe6c2bb80ff5f10832a21407f7
aebabis <acbabis@gmail.com>
2026-06-01 11:02:52 -0700

    Removing hacknet servers from nmap

--- home/lib/nmap.ts
+++ home/lib/nmap.ts
@@ -4,7 +4,9 @@ export const nmap = (ns: NS) => {
   const hostnames = new Set(['home']);
   for (const hostname of hostnames)
     for (const neighbor of ns.scan(hostname)) hostnames.add(neighbor);
-  return [...hostnames];
+  return [...hostnames].filter(
+    (hostname) => !hostname.startsWith('hacknet-server'),
+  );
 };
 
 export const saveHostnames = (ns: NS) => putHostnames(ns, nmap(ns));
