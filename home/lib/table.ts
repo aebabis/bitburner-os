6fe0198d0b227199889cfdcdefa129a099883b29
aebabis <acbabis@gmail.com>
2026-05-31 20:03:10 -0700

    More TS

--- home/lib/table.ts
+++ home/lib/table.ts
@@ -14,8 +14,7 @@ type TableColumn = {
 };
 
 const headData = (column: ColumnSpec): TableColumn => {
-  const spec =
-    /** @type {ColumnSpec} */ typeof column === 'string' ? {} : column;
+  const spec: ColumnSpec = typeof column === 'string' ? {} : column;
   const name = spec.name != null ? spec.name : /** @type {string} */ column;
   const align = spec.align || 'left';
   const process = spec.process || ((x: string | number) => x);
@@ -38,13 +37,12 @@ const headData = (column: ColumnSpec): TableColumn => {
   };
 };
 
-/** @param {string[][]} lines @param {number} numCols */
-export const transpose = (lines, numCols) => {
+export const transpose = (lines: string[][], numCols: number) => {
   const numRows = Math.ceil(lines.length / numCols);
-  const cols = /** @type {string[][][]} */ [];
+  const cols: string[][][] = [];
   for (let i = 0; i < numCols; i++) cols.push(lines.splice(0, numRows));
 
-  const rows = /** @type {string[][]} */ [];
+  const rows: string[][] = [];
   while (cols[0].length > 0) {
     const row = cols
       .map((col, i) => {
@@ -53,21 +51,24 @@ export const transpose = (lines, numCols) => {
         return section;
       })
       .flat();
-    rows.push(/** @type {string[]} */ row);
+    rows.push(row);
   }
   return rows;
 };
 
-/** @param {NS} ns @param {(string | ColumnSpec)[] | null} columns @param {(string | number | undefined)[][]} data @param {{ borders?: boolean, colors?: boolean }} [options] */
-export const table = (ns, columns, data, options = {}) => {
+export const table = (
+  ns: NS,
+  columns: (string | ColumnSpec)[],
+  data: (string | number | undefined)[][],
+  options: { borders?: boolean; colors?: boolean } = {},
+) => {
   const { borders = false, colors = false } = options;
   const joiner = borders ? ' | ' : '  ';
-  const head = colors ? BRIGHT.BOLD : (/** @type {string} */ s) => s;
-  /** @type {TableColumn[]} */
-  let processedColumns;
+  const head = colors ? BRIGHT.BOLD : (s: string) => s;
+  let processedColumns: TableColumn[];
   if (columns == null) {
     if (data.length === 0) return '';
-    processedColumns = /** @type {string[]} */ data.shift().map(headData);
+    processedColumns = data.shift()!.map(headData);
   } else {
     processedColumns = columns.map(headData);
   }
