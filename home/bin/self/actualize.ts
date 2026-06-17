7014ba174d050f52d63776a68284d44e645eeda8
aebabis <acbabis@gmail.com>
2026-06-16 17:45:27 -0700

    Giving actualize.ts control of BitNode sequence

--- home/bin/self/actualize.ts
+++ home/bin/self/actualize.ts
@@ -1,4 +1,27 @@
+const BN_SEQUENCE: [number, number][] = [
+  [1, 1],
+  [4, 1],
+  [4, 2],
+  [1, 2],
+  [4, 3],
+
+  [5, 1],
+  [2, 1],
+  [3, 1],
+
+  [1, 3],
+
+  [6, 1],
+  [6, 2],
+  [6, 3],
+
+  [7, 1],
+  [7, 2],
+  [7, 3],
+];
+
 export async function main(ns: NS) {
-  const [bn, script] = ns.args;
-  ns.singularity.destroyW0r1dD43m0n(+bn, script as string);
+  const { ownedSF } = ns.getResetInfo();
+  const [nextBN] = BN_SEQUENCE.find(([bn, level]) => (ownedSF.get(bn) ?? 0) < level) ?? [12];
+  ns.singularity.destroyW0r1dD43m0n(nextBN, 'start.ts');
 }
