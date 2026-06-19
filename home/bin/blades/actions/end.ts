4e57d71a2fb6439704d1c064b6b5b3ffeba875d4
aebabis <acbabis@gmail.com>
2026-06-19 15:58:10 -0700

    Cleanup

--- home/bin/blades/actions/end.ts
+++
@@ -1,7 +0,0 @@
-import { joinSpawnChain } from '../../../lib/service-api';
-
-export async function main(ns: NS) {
-  const { linkTo } = joinSpawnChain(ns, '/bin/blades/blades.ts');
-  await ns.bladeburner.nextUpdate();
-  await linkTo('/bin/blades/load-actions.ts', 0);
-}
