6e7835eb469fc97d9f71e337540c013498741c7d
aebabis <acbabis@gmail.com>
2026-06-21 15:32:58 -0700

    Cleanup

--- home/bin/infect.ts
+++ home/bin/infect.ts
@@ -7,9 +7,3 @@ export const fullInfect = (ns: NS, ...hostnames: string[]) => {
   const JS = ns.ls('home').filter((f) => f.endsWith('.ts'));
   for (const hostname of hostnames) ns.scp(JS, hostname, 'home');
 };
-
-export async function main(ns: NS) {
-  if (ns.args.some((arg) => typeof arg !== 'string'))
-    throw new Error('Args list contains a non string: ' + ns.args);
-  return infect(ns, ...(ns.args as string[]));
-}
