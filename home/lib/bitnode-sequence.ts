439f92c2b5696f176521e1937059109b9833104a
aebabis <acbabis@gmail.com>
2026-06-21 15:06:39 -0700

    Extracting BN sequence

---
+++ home/lib/bitnode-sequence.ts
@@ -0,0 +1,29 @@
+const BN_SEQUENCE: [number, number][] = [
+  [1, 1],
+  [4, 2],
+  [1, 2],
+  [4, 3],
+
+  [5, 1],
+  [2, 1],
+  [1, 3],
+
+  [3, 1],
+
+  [12, 5],
+
+  [6, 1],
+  [6, 2],
+  [6, 3],
+
+  [7, 1],
+  [7, 2],
+  [12, 8],
+  [7, 3],
+];
+
+export const getNextBitnode = (resetInfo: ResetInfo) => {
+  const { ownedSF } = resetInfo;
+  const nextUnmet = BN_SEQUENCE.find(([bn, level]) => (ownedSF.get(bn) ?? 0) < level);
+  return nextUnmet?.[0] ?? 12;
+};
