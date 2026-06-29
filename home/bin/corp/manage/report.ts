eba54d64ee52654adf949e7af6fdf44c37ff3d9e
aebabis <acbabis@gmail.com>
2026-06-29 15:21:59 -0700

    Dead code removal

--- home/bin/corp/manage/report.ts
+++
@@ -1,9 +0,0 @@
-import { putCorpReports } from '../../../lib/data-store';
-
-export const startReport = (ns: NS, division: string) => {
-  const entries = [[division.toUpperCase()]];
-  return {
-    add: (content: string[]) => entries.push(content),
-    send: () => putCorpReports(ns, { [division]: entries }),
-  };
-};
