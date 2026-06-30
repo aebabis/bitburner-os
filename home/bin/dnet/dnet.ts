70b541a201a0ea21b12b9861a57b28c5c2e22c5c
aebabis <acbabis@gmail.com>
2026-06-30 14:59:02 -0700

    Making mole auto-update/propagate

--- home/bin/dnet/dnet.ts
+++ home/bin/dnet/dnet.ts
@@ -1,8 +1,6 @@
 /*
  NAME                                 RAM     
- dnet.connectToSession                0.05GB  
  dnet.heartbleed                      0.60GB  
- dnet.openCache                       2.00GB  
  dnet.setStasisLink                   12.00GB 
  dnet.getStasisLinkLimit              0.00GB  
  dnet.getStasisLinkedServers          0.00GB  
@@ -17,10 +15,14 @@
  dnet.getServerRequiredCharismaLevel  0.10GB  
  */
 
+import { ERROR } from '../../lib/colors';
+
 /* USED
- dnet.authenticate                    0.40GB  
+ dnet.openCache                       2.00GB  
  dnet.probe                           0.20GB  
  dnet.getServerDetails                0.10GB  
+ dnet.authenticate                    0.40GB  
+ dnet.connectToSession                0.05GB  
  dnet.nextMutation                    0.00GB  
 */
 
@@ -30,19 +32,85 @@
  dnet.labradar                        0.00GB 
  */
 
+const DARKWEB = 'darkweb';
+
+const getVersioner = (ns: NS, filename: string) => {
+  filename = filename.replace(/^\//, '');
+
+  const baseName = filename.replace(/\.ts/, '');
+  const installedVersions = ns.ls(DARKWEB).filter((name) => name.startsWith(baseName));
+  const priorVersions = installedVersions
+    .map((name) => name.split('-').pop()!)
+    .map((name) => name.split('.').shift()!)
+    .map((name) => name.match(/\d+/) || ['1'])
+    .map(([version]) => +version);
+
+  let version = Math.max(1, ...priorVersions);
+  let content = '';
+
+  ns.print('Checking version');
+  ns.print(`  Installed versions of ${filename}: ${installedVersions}`);
+  ns.print(`  Identified version numbers: ${priorVersions.join(', ')}`);
+  ns.print(`  Latest version: ${version}`);
+
+  const getVersionedName = () =>
+    filename.replace(/([^/]+)\.ts$/, (_, name) => `${name}-v${version}.ts`);
+  const downloadLatest = () => {
+    const recentVersion = getVersionedName();
+    const latest = ns.read(filename);
+    if (latest === '') {
+      ns.print(ERROR + 'Current state of base program not runnable. Skipping');
+    } else if (content !== latest) {
+      version++;
+      ns.print(
+        content === ''
+          ? `  Installing current state as new version: ${version}`
+          : `New version detected: ${version}`,
+      );
+      const newVersion = getVersionedName();
+      ns.write(newVersion, latest);
+      ns.scp(newVersion, DARKWEB, 'home');
+      ns.rm(newVersion, 'home');
+      content = latest;
+    }
+    return {
+      baseName,
+      version,
+      recent: recentVersion,
+      current: getVersionedName(),
+    };
+  };
+
+  return downloadLatest;
+};
+
 export async function main(ns: NS) {
+  ns.disableLog('ALL');
+  if (ns.getHostname() !== 'home') {
+    const name = ns.getScriptName().split('/').pop()!;
+    throw new Error(`darknet launcher (${name}) assumes it runs on home`);
+  }
+  const getVersions = getVersioner(ns, '/bin/dnet/mole.ts');
+
   ns.ui.openTail();
 
   while (true) {
-    ns.clearLog();
-    for (const ps of ns.ps('darkweb')) {
-      if (ps.filename.includes('mole.ts')) {
-        ns.ui.closeTail(ps.pid);
-        ns.kill(ps.pid);
+    const { baseName, recent, current } = getVersions();
+    if (current !== recent) {
+      ns.print('Killing old versions');
+      const runningVersions = ns.ps(DARKWEB).filter((ps) => ps.filename.startsWith(baseName));
+      for (const ps of runningVersions) {
+        if (ps.filename !== current) {
+          ns.print('  ' + ps.filename);
+          ns.ui.closeTail(ps.pid);
+          ns.kill(ps.pid);
+        }
       }
     }
-    ns.scp(ns.ls('home', '/bin/dnet/'), 'darkweb', 'home');
-    ns.exec('/bin/dnet/mole.ts', 'darkweb', 1, Date.now());
+    if (!ns.ps(DARKWEB).find((ps) => ps.filename === current)) {
+      ns.print('Launching current version: ' + current);
+      ns.exec(current, DARKWEB, 1);
+    }
     await ns.dnet.nextMutation();
   }
 }
--- home/bin/dnet/mole.ts
+++ home/bin/dnet/mole.ts
@@ -30,32 +30,47 @@ function romanToInt(numeralString: string) {
     .reduce((a, b) => a + b, 0);
 }
 
-const NO_PASSWORD = ['There is no password', 'The password is not set', 'The PIN is empty'];
+const NO_PASSWORD = [
+  'There is no password',
+  'The password is not set',
+  'The PIN is empty',
+  'Did I set a code?',
+];
 const DEFAULT_PASSWORD = [
   'I never changed the password',
   'The default password is set',
   "It's still the factory settings",
   "It's still the default",
 ];
-const PASSWORD_IS = ['The secret is', 'The password is', 'Remember to use'];
+const PASSWORD_IS = [
+  'The secret is ',
+  'The password is ',
+  'Remember to use ',
+  'The key is ',
+  'The PIN is ',
+  "It's set to ",
+];
 
 const getPassword = (details: DarknetServerDetails) => {
+  if (details.passwordLength === 0) return '';
+
   if (PASSWORD_IS.some((text) => details.passwordHint.startsWith(text))) {
-    return details.data;
+    return details.data || details.passwordHint.split(' ').pop()!;
   }
   if (NO_PASSWORD.includes(details.passwordHint)) {
     return '';
   }
-  if (DEFAULT_PASSWORD.includes(details.passwordHint)) {
+  if (DEFAULT_PASSWORD.includes(details.passwordHint) || details.passwordHint.includes('default')) {
     if (details.passwordLength === 0) return '';
     if (details.passwordLength === 4) return '0000';
-    if (details.passwordLength === 5) return 'admin';
+    if (details.passwordLength === 5) return Math.random() < 0.5 ? '12345' : 'admin';
     if (details.passwordLength === 8) return 'password';
   }
   if (details.passwordHint === 'Type the numbers to prove you are human') {
     return details.data.replaceAll(/[^0-9]/g, '');
   }
   if (details.passwordHint.startsWith('The password is the value of the number')) {
+    console.log(details.data, romanToInt(details.data));
     return romanToInt(details.data).toString();
   }
   return null;
@@ -63,12 +78,26 @@ const getPassword = (details: DarknetServerDetails) => {
 
 const authenticate = async (ns: NS, hostname: string, details: DarknetServerDetails) => {
   const password = getPassword(details);
-  if (password) {
-    if (ns.dnet.connectToSession(hostname, password)) return true;
-    const result = await ns.dnet.authenticate(hostname, password);
-    return result.success;
+  if (password == null) {
+    ns.print('No password strategy for: ' + hostname);
+  } else {
+    if (ns.dnet.connectToSession(hostname, password).success) {
+      ns.print(`${hostname}: "${password}", (reconnected)`);
+      return true;
+    } else {
+      const result = await ns.dnet.authenticate(hostname, password);
+      if (result.success) {
+        ns.print(`${hostname}: "${password}", (connected)`);
+        return true;
+      } else {
+        ns.print(`ERROR${hostname}: "${password}", (failed)`);
+        return false;
+      }
+    }
   }
 };
+
+const getVersion = (script: string) => parseInt(script.split('-v').pop()!) || 0;
 
 export async function main(ns: NS) {
   const caches = ns.ls(ns.getHostname(), '.cache');
@@ -76,9 +105,10 @@ export async function main(ns: NS) {
     ns.tprint(ns.dnet.openCache(cache));
   }
   if (ns.getHostname() === 'darkweb') {
-    // ns.ui.openTail();
-    // ns.ui.moveTail(250, 2);
-    // ns.ui.resizeTail(700, 500);
+    ns.disableLog('ALL');
+    ns.ui.openTail();
+    ns.ui.moveTail(250, 2);
+    ns.ui.resizeTail(700, 500);
     // ns.print('labreport');
     // ns.print(await ns.dnet.labreport());
     // ns.print('labrader');
@@ -90,11 +120,24 @@ export async function main(ns: NS) {
     for (const hostname of connections) {
       const details = ns.dnet.getServerDetails(hostname);
       if (details.hasSession || (await authenticate(ns, hostname, details))) {
-        for (const ps of ns.ps(hostname)) {
-          if (ns.args[0] > (ps.args[0] ?? 0)) ns.kill(ps.pid);
+        const otherMoles = ns.ps(hostname).filter((ps) => ps.filename.includes('mole'));
+        if (otherMoles.length === 0) {
+          ns.scp(ns.getScriptName(), hostname);
+          ns.exec(ns.getScriptName(), hostname);
+        } else {
+          for (const ps of otherMoles) {
+            const version = getVersion(ns.getScriptName());
+            const otherVersion = getVersion(ps.filename);
+            if (version < otherVersion) return;
+            if (version > otherVersion) {
+              ns.kill(ps.pid);
+              if (ns.isRunning(ns.getScriptName(), hostname)) {
+                ns.scp(ns.getScriptName(), hostname);
+                ns.exec(ns.getScriptName(), hostname, 1, ns.args[0]);
+              }
+            }
+          }
         }
-        ns.scp(ns.ls(ns.getHostname(), '/bin/dnet/'), hostname);
-        ns.exec('/bin/dnet/mole.ts', hostname);
       }
       await ns.dnet.heartbleed(hostname);
     }
