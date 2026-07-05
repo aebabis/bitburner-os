04e167339c15cf971afe257b2afde2610a1cd508
aebabis <acbabis@gmail.com>
2026-07-05 00:49:19 -0700

    Auto-closing tails

--- home/stop.ts
+++ home/stop.ts
@@ -1,6 +1,7 @@
 import { nmap } from './lib/nmap';
 import { tprint } from './boot/util';
 import { GRAY, STR } from './lib/colors';
+import { getServices } from './lib/service-api';
 
 export const stop = async (ns: NS) => {
   // Since batching can create several 1000 threads, this routine
@@ -11,6 +12,12 @@ export const stop = async (ns: NS) => {
   // needing to yield the thread.
 
   tprint(ns)(STR.BOLD + 'SHUTTING DOWN');
+
+  // Close service tails to prevent clutter
+  const services = getServices(ns);
+  for (const service of services) {
+    if (service.pid != null) ns.ui.closeTail(service.pid);
+  }
 
   const hostnames = nmap(ns);
 
