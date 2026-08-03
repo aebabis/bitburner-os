c3b75b8a7c88744e0bb4c8a4ca0a2fef0f01479e
aebabis <acbabis@gmail.com>
2026-08-03 11:52:24 -0700

    Dead code removal

--- home/bin/self/hack.ts
+++
@@ -1,9 +0,0 @@
-export async function main(ns: NS) {
-  try {
-    ns.singularity.connect('n00dles');
-    ns.print('Hacking n00dles...');
-    ns.print(await ns.singularity.manualHack());
-  } catch (error) {
-    console.error(error);
-  }
-}
