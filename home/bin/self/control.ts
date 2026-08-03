49abf40a04fedba9e57adb2d8a4881920a8ebd07
aebabis <acbabis@gmail.com>
2026-08-03 10:28:42 -0700

    Giving control.ts better hack targets and removing RMI use

--- home/bin/self/control.ts
+++ home/bin/self/control.ts
@@ -1,16 +1,51 @@
-import { getPath } from '../../lib/backdoor.ts';
-import { rmi } from '../../lib/rmi';
+import { ERROR } from '../../lib/colors.ts';
+import { getMoneyData } from '../../lib/data-store.ts';
+import { inPlace, runInPlace } from '../../lib/in-place.ts';
+import { $nmap } from '../../lib/nmap.rip.ts';
+import { $getBackdoorPath } from '../../lib/backdoor.rip.ts';
+import { formatTime } from '../../lib/util.ts';
 
 export async function main(ns: NS) {
   ns.disableLog('ALL');
+
+  // Reserve RAM
+  ns.singularity.connect;
+  ns.scan;
+  ns.getHackingLevel;
+
+  const $ = inPlace(ns, ns.pid);
+  const $rip = runInPlace(ns, ns.pid);
+
   while (true) {
-    const path = getPath(ns);
-    if (path != null) {
-      if (path[path.length - 1] !== 'w0r1d_d43m0n') {
-        await rmi(ns)('/bin/self/backdoor.ts', 1, ...path);
+    const hostnames = await $nmap(ns, ns.pid)();
+    const path = await $getBackdoorPath(ns, ns.pid)(hostnames);
+    try {
+      if (path != null && path.at(-1) !== 'w0r1d_d43m0n') {
+        await $rip((path: string[]) => {
+          for (const hostname of path) ns.singularity['connect'](hostname);
+        })(path);
+        ns.print(`Backdooring: ${path.at(-1)}`);
+        await $.singularity['installBackdoor']();
+      } else {
+        const { theft } = getMoneyData(ns);
+        const hostname = theft?.target;
+        if (hostname != null) {
+          await $rip((path: string[]) => {
+            for (const hostname of path) ns.singularity['connect'](hostname);
+          })([hostname]);
+          const expectedTime = ns.getHackTime(hostname) / 1000;
+          ns.print(`Hacking:     ${hostname} (${formatTime(expectedTime)})`);
+          const start = Date.now();
+          const money = await $.singularity['manualHack']();
+          const end = Date.now();
+          const s = (end - start) / 1000;
+          ns.print(`  $${ns.format.number(money)}, ${formatTime(s)}`);
+          ns.print(`  $${ns.format.number(money / s)}/s`);
+        }
       }
-    } else {
-      await rmi(ns)('/bin/self/hack.ts');
+    } catch (error) {
+      ns.print(ERROR + error);
+      ns.print(ERROR + '  (player probably started using terminal)');
     }
     await ns.sleep(100);
   }
